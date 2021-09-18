7af91963d514e082d229af01479c9afeb6db6575
bspeelmeyer <bspeelmeyer@gmail.com>
2021-09-18 15:52:23 +1000

    commit from other repo

---
+++ express/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./src/database");
+
+// Sync the database in the background
+db.sync();
+
+const app = express();
+
+// Parse requests of content-type - application.json
+app.use(express.json());
+
+// Add cors support
+app.use(cors());
+
+// Implement routes here
+
+// Set port, listen for requests
+const PORT = 4500;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}.`);
+});
